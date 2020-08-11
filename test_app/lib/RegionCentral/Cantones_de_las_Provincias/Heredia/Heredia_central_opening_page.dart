b771917d120a33a48f9eed0c178ba99e53d58e94
DekuCool24 <dayanna.qs@gmail.com>
2020-08-10 22:40:55 -0600

    Create Heredia_central_opening_page.dart

---
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Heredia_central_opening_page.dart
@@ -0,0 +1,149 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Barva/Barva_Heredia_central_opening_page.dart';
+import 'Belén/Belen_Heredia_central_opening_page.dart';
+import 'Flores/Flores_Heredia_central_opening_page.dart';
+import 'Heredia/Heredia_Heredia_central_opening_page.dart';
+import 'San_Isidro/San_Isidro_Heredia_central_opening_page.dart';
+import 'San_Pablo/San_Pablo_Heredia_central_opening_page.dart';
+import 'San_Rafael/San_Rafael_Heredia_central_opening_page.dart';
+import 'Santa_Bárbara/Santa_Barbara_Heredia_central_opening_page.dart';
+import 'Santo_Domingo/Santo_Domingo_Heredia_central_opening_page.dart';
+
+class regionSocialEconomicaCentralHeredia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Cantones de la Provincia de Heredia',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('Barva', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralBarvaHeredia()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Belén', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralBelenHeredia()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Flores', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralFloresHeredia()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Heredia', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralHerediaHeredia()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Isidro', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralSanIsidroHeredia()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Pablo', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralSanPabloHeredia()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Rafael', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralSanRafaelHeredia()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Santa Bárbara', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralSantaBarbaraHeredia()),
+              )
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Santo Domingo', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => regionSocialEconomicaCentralSantoDomingoHeredia()),
+              )
+            },
+          ),
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('StoryTelling de la Provincia', style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}
