7804940ce209f3e4b9dc4f65c519826cd9d1e775
DekuCool24 <dayanna.qs@gmail.com>
2020-08-10 22:40:52 -0600

    Create Flores_Heredia_central_opening_page.dart

---
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Flores/Flores_Heredia_central_opening_page.dart
@@ -0,0 +1,57 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class regionSocialEconomicaCentralFloresHeredia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Distritos del Cantón de Flores',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('Llorente', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Joaquín', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Lorenzo', style: TextStyle(fontSize: 25),
+            ),
+          ),
+
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}
