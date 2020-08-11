1467c5c37c0dd5e781e0119ac34c85630a2121c0
DekuCool24 <dayanna.qs@gmail.com>
2020-08-10 22:39:58 -0600

    Create Valverde_Vegas_Alajuela_central_opening_page.dart

---
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Valverde_Vega/Valverde_Vegas_Alajuela_central_opening_page.dart
@@ -0,0 +1,118 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Valverde_Vega_Alajuela_Storytelling.dart';
+
+class regionSocialEconomicaCentralValverdeVegaAlajuela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Distritos del Cantón de Valverde Vega',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('Rodríguez', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralRodriguezValverdeVegaAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Pedro', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanPedroValverdeVegaAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Sarchí Norte', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSarchiNorteValverdeVegaAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Sarchí Sur', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSarchiSurValverdeVegaAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Toro Amarillo', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralToroAmarilloValverdeVegaAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Gráfico del Cantón', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingValverdeVegaAlajuela.withSampleData()),
+              )
+            },
+          ),
+        ],
+      ),
+    );
+
+  }
+}
