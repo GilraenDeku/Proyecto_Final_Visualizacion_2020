9fa737f6965a8feaa22750a23a27a4a5ee515ef7
DekuCool24 <dayanna.qs@gmail.com>
2020-08-10 22:38:05 -0600

    Create Naranjo_Alajuela_central_opening_page.dart

---
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Naranjo_Alajuela_central_opening_page.dart
@@ -0,0 +1,160 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Naranjo_Alajuela_StoryTelling.dart';
+
+class regionSocialEconomicaCentralNaranjoAlajuela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Distritos del Cantón de Naranjo',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('Cirrí', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralCirriNaranjoAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Naranjo', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralNaranjoNaranjoAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Palmitos', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralPalmitosNaranjoAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Rosario', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralRosarioNaranjoAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Jerónimo', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJeronimoNaranjoAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Juan', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJuanNaranjoAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Juanillo', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJuanilloNaranjoAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Miguel', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanMiguelNaranjoAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('StoryTelling del Cantón', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingNaranjoAlajuela.withSampleData()),
+              )
+            },
+          ),
+        ],
+      ),
+    );
+
+  }
+}
