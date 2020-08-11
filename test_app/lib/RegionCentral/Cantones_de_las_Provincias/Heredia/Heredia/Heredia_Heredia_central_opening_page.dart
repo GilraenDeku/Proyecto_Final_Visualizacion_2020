e4eae138a1378cf9dedb470a2d37a34bafcae311
DekuCool24 <dayanna.qs@gmail.com>
2020-08-10 22:40:53 -0600

    Create Heredia_Heredia_central_opening_page.dart

---
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Heredia/Heredia_Heredia_central_opening_page.dart
@@ -0,0 +1,67 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class regionSocialEconomicaCentralHerediaHeredia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Distritos del Cantón de Heredia',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('Heredia', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Mercedes', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Francisco', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Ulloa', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Vara Blanca', style: TextStyle(fontSize: 25),
+            ),
+          ),
+
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}
