39331e720293d0d0e2a10f3baeb5206415147826
DekuCool24 <dayanna.qs@gmail.com>
2020-08-10 22:38:43 -0600

    Create Poas_Alajuela_central_opening_page.dart

---
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Poás/Poas_Alajuela_central_opening_page.dart
@@ -0,0 +1,118 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Poas_Alajuela_Storytelling.dart';
+
+class regionSocialEconomicaCentralPoasAlajuela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Distritos del Cantón de Poás',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('Carrillos', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralCarrillosPoasAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Sabana Redonda', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSabanaRedondaPoasAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Juan', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJuanPoasAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Pedro', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanPedroPoasAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Rafael', style: TextStyle(fontSize: 25),
+            ),
+            /*
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanRafaelPoasAlajuela.withSampleData()),
+              )
+            },
+
+             */
+          ),
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('StoryTelling del Cantón', style: TextStyle(fontSize: 25),
+            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingPoasAlajuela.withSampleData()),
+              )
+            },
+          ),
+        ],
+      ),
+    );
+
+  }
+}
