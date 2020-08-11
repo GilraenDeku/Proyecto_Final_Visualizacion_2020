1dcad8fe7b4449a790798ee860e7685ccc28d0cd
DekuCool24 <dayanna.qs@gmail.com>
2020-08-10 22:40:27 -0600

    Create Jimenez_Cartago_central_opening_page.dart

---
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Jiménez/Jimenez_Cartago_central_opening_page.dart
@@ -0,0 +1,61 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class regionSocialEconomicaCentralJimenezCartago extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Distritos del Cantón de Jimenez',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('El Tejar', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Patio de Agua', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Isidro', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Tobosi', style: TextStyle(fontSize: 25),
+            ),
+          ),
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}
