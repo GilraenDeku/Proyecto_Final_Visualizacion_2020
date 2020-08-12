f9994073e291bf754f962b9ff020894ecbbb09b0
DekuCool24 <dayanna.qs@gmail.com>
2020-08-11 23:25:05 -0600

    Update Grecia_Alajuela_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/Grecia_Alajuela_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/Grecia_Alajuela_central_opening_page.dart
@@ -8,6 +8,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/Bol%C3%ADvar/Bolivar_Grecia_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/Grecia/Grecia_Grecia_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/Puente_de_Piedra/Puente_de_Piedra_Grecia_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/San_Isidro/San_Isidro_Grecia_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/San_Jos%C3%A9/San_Jose_Grecia_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/San_Roque/San_Roque_Grecia_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Grecia/Tacares/Tacares_Grecia_Alajuela_StoryTelling.dart';
 import 'Grecia_Alajuela_Storytelling.dart';
 
 class regionSocialEconomicaCentralGreciaAlajuela extends StatelessWidget {
@@ -32,99 +39,78 @@ class regionSocialEconomicaCentralGreciaAlajuela extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Bolívar', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralBolivarGreciaAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingBolivarGreciaAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Grecia', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralGreciaGreciaAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingGreciaGreciaAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Puente de Piedra', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralPuentedePiedraGreciaAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingPuentedePiedraGreciaAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Isidro', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanIsidroGreciaAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanIsidroGreciaAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San José', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJoseGreciaAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanJoseGreciaAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Roque', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanRoqueGreciaAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanRoqueGreciaAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Tacares', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralTacaresGreciaAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingTacaresGreciaAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
 
           ListTile(
