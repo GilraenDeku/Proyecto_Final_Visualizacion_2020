2518174dbe4fd65ce383fd7b9f8e655f47c5722f
DekuCool24 <dayanna.qs@gmail.com>
2020-08-11 23:25:49 -0600

    Update Poas_Alajuela_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Poás/Poas_Alajuela_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Poás/Poas_Alajuela_central_opening_page.dart
@@ -8,6 +8,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Po%C3%A1s/Carrillos/Carrillos_Poas_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Po%C3%A1s/Sabana_Redonda/Sabana_Redonda_Poas_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Po%C3%A1s/San_Juan/San_Juan_Poas_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Po%C3%A1s/San_Pedro/San_Pedro_Poas_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Po%C3%A1s/San_Rafael/San_Rafael_Poas_Alajuela_StoryTelling.dart';
 import 'Poas_Alajuela_Storytelling.dart';
 
 class regionSocialEconomicaCentralPoasAlajuela extends StatelessWidget {
@@ -32,71 +37,56 @@ class regionSocialEconomicaCentralPoasAlajuela extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Carrillos', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralCarrillosPoasAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingCarrillosPoasAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Sabana Redonda', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSabanaRedondaPoasAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSabanaRedondaPoasAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Juan', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJuanPoasAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanJuanPoasAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Pedro', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanPedroPoasAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanPedroPoasAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Rafael', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanRafaelPoasAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanRafaelPoasAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
 
           ListTile(
