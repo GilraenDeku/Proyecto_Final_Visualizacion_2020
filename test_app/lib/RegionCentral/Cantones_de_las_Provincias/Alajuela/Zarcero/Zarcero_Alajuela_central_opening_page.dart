c6e0d90f7ca264d7fbf923d8cac962f9e5559f42
DekuCool24 <dayanna.qs@gmail.com>
2020-08-11 23:26:44 -0600

    Update Zarcero_Alajuela_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Zarcero_Alajuela_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Zarcero_Alajuela_central_opening_page.dart
@@ -8,6 +8,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Brisas/Brisas_Zarcero_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Guadalupe/Guadalupe_Zarcero_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Laguna/Laguna_Zarcero_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Palmira/Palmira_Zarcero_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Tapesco/Tapezco_Zarcero_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Zapote/Zapote_Zarcero_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Zarcero/Zarcero/Zarcero_Zarcero_Alajuela_StoryTelling.dart';
 import 'Zarcero_Alajuela_StoryTelling.dart';
 
 class regionSocialEconomicaCentralZarceroAlajuela extends StatelessWidget {
@@ -32,99 +39,78 @@ class regionSocialEconomicaCentralZarceroAlajuela extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Brisas', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralBrisasZarceroAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingBrisasZarceroAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Guadalupe', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralGuadalupeZarceroAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingGuadalupeZarceroAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Laguna', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralLagunaZarceroAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingLagunaZarceroAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Palmira', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralPalmiraZarceroAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingPalmiraZarceroAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
-            title: Text('Tapezco', style: TextStyle(fontSize: 25),
+            title: Text('Tapesco', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralTapezcoZarceroAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingTapescoZarceroAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Zapote', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralZapoteZarceroAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingZapoteZarceroAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Zarcero', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralZapoteZarceroAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingZarceroZarceroAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
 
           ListTile(
