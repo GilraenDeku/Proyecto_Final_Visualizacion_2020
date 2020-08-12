c9fa352b994c384b162ba01e37c8f00a5e7613f8
DekuCool24 <dayanna.qs@gmail.com>
2020-08-11 23:25:20 -0600

    Update Naranjo_Alajuela_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Naranjo_Alajuela_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Naranjo_Alajuela_central_opening_page.dart
@@ -8,6 +8,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Cirr%C3%AD/Cirri_Naranjo_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Naranjo/Naranjo_Naranjo_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Palmitos/Palmitos_Naranjo_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Rosario/Rosario_Naranjo_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/San_Jer%C3%B3nimo/San_Jeronimo_Naranjo_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/San_Juan/San_Juan_Naranjo_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/San_Juanillo/San_Juanillo_Naranjo_Alajuela_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/San_Miguel/San_Miguel_Naranjo_Alajuela_StoryTelling.dart';
 import 'Naranjo_Alajuela_StoryTelling.dart';
 
 class regionSocialEconomicaCentralNaranjoAlajuela extends StatelessWidget {
@@ -32,113 +40,89 @@ class regionSocialEconomicaCentralNaranjoAlajuela extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Cirrí', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralCirriNaranjoAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingCirriNaranjoAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Naranjo', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralNaranjoNaranjoAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingNaranjoNaranjoAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Palmitos', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralPalmitosNaranjoAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingPalmitosNaranjoAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Rosario', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralRosarioNaranjoAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingRosarioNaranjoAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Jerónimo', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJeronimoNaranjoAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanJeronimoNaranjoAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Juan', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJuanNaranjoAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanJuanNaranjoAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Juanillo', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanJuanilloNaranjoAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanJuanilloNaranjoAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Miguel', style: TextStyle(fontSize: 25),
             ),
-            /*
             onTap: () => {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => GraficaRegionCentralSanMiguelNaranjoAlajuela.withSampleData()),
+                MaterialPageRoute(builder: (context) => storytellingSanMiguelNaranjoAlajuela.withSampleData()),
               )
             },
-
-             */
           ),
 
           ListTile(
