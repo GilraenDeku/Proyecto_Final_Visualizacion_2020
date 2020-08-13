35fc2ad7691ecf961011bd12228d381e664c600a
DekuCool24 <dayanna.qs@gmail.com>
2020-08-12 21:11:42 -0600

    Update Poas_Alajuela_Storytelling.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Poás/Poas_Alajuela_Storytelling.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Poás/Poas_Alajuela_Storytelling.dart
@@ -130,6 +130,7 @@ class storytellingPoasAlajuela extends StatelessWidget {
         children: [
           _buildButtonColumn(color, Icons.find_in_page, 'WEB'),
           _buildMuniButtonColumn(color, Icons.find_in_page, 'MUNICIPALIDAD'),
+          _buildMapaButtonColumn(color, Icons.find_in_page, 'MAPA'),
         ],
       ),
     );
@@ -410,6 +411,47 @@ class storytellingPoasAlajuela extends StatelessWidget {
                 onTap: () {
                   // do what you need to do when "Click here" gets clicked
                   launch('http://poasdigital.go.cr/');
+                }
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column _buildMapaButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+        GestureDetector(
+            child:
+            Icon(icon, color: color),
+            onTap: () {
+              // do what you need to do when "Click here" gets clicked
+              launch('https://www.hacienda.go.cr/docs/58123b39bee70_Mapa%20'
+                  'del%20Canton%20Poas%2008%20Distrito%2001%20a%2005.pdf');
+            }
+        ),
+
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Center(
+            child:
+            GestureDetector(
+                child: Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: color,
+                  ),
+                ),
+                onTap: () {
+                  // do what you need to do when "Click here" gets clicked
+                  launch('https://www.hacienda.go.cr/docs/58123b39bee70_Mapa%20'
+                      'del%20Canton%20Poas%2008%20Distrito%2001%20a%2005.pdf');
                 }
             ),
           ),
