792fa20f62db528c80fc28aab36e3990ed57f321
DekuCool24 <dayanna.qs@gmail.com>
2020-08-12 21:11:38 -0600

    Update Naranjo_Alajuela_StoryTelling.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Naranjo_Alajuela_StoryTelling.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/Naranjo/Naranjo_Alajuela_StoryTelling.dart
@@ -130,6 +130,7 @@ class storytellingNaranjoAlajuela extends StatelessWidget {
         children: [
           _buildButtonColumn(color, Icons.find_in_page, 'WEB'),
           _buildMuniButtonColumn(color, Icons.find_in_page, 'MUNICIPALIDAD'),
+          _buildMapaButtonColumn(color, Icons.find_in_page, 'MAPA'),
         ],
       ),
     );
@@ -417,6 +418,50 @@ class storytellingNaranjoAlajuela extends StatelessWidget {
       ],
     );
   }
+
+  Column _buildMapaButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+        GestureDetector(
+            child:
+            Icon(icon, color: color),
+            onTap: () {
+              // do what you need to do when "Click here" gets clicked
+              launch('https://www.hacienda.go.cr/docs/58123b4dc9089_'
+                  'Mapa%20del%20Canton%20Naranjo%2006%20'
+                  'Distrito%2001%20a%2008.pdf');
+            }
+        ),
+
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Center(
+            child:
+            GestureDetector(
+                child: Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: color,
+                  ),
+                ),
+                onTap: () {
+                  // do what you need to do when "Click here" gets clicked
+                  launch('https://www.hacienda.go.cr/docs/58123b4dc9089_Mapa'
+                      '%20del%20Canton%20Naranjo%2006%20Distrito'
+                      '%2001%20a%2008.pdf');
+                }
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
 
 }
 
