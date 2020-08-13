f2e6a9c64cdf86a8810af51f061fbe7de1aee84e
DekuCool24 <dayanna.qs@gmail.com>
2020-08-12 21:13:58 -0600

    Update Paraiso_Cartago_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Paraiso_Cartago_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Paraiso_Cartago_central_opening_page.dart
@@ -8,6 +8,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Cach%C3%AD/Cachi_Paraiso_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Llanos_de_Santa_Luc%C3%ADa/Llanos_de_Santa_Luc%C3%ADa_Paraiso_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Orosi/Orosi_Paraiso_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Paraiso/Paraiso_Paraiso_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Paraiso_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Santiago/Santiago_Paraiso_Cartago_StoryTelling.dart';
 
 class regionSocialEconomicaCentralParaisoCartago extends StatelessWidget {
   @override
@@ -31,32 +37,68 @@ class regionSocialEconomicaCentralParaisoCartago extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Cachí', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingCachiParaisoCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Llanos de Santa Lucía', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingLlanosdeSantaLuciaParaisoCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Orosi', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingOrosiParaisoCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Paraiso', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingParaisoParaisoCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santiago', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantiagoParaisoCartago.withSampleData()),
+              )
+            },
           ),
 
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingParaisoCartago.withSampleData()),
+              )
+            },
           ),
         ],
       ),
