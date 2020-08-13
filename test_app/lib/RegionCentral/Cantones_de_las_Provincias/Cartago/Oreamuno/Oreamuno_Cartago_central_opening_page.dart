41e05f20a0e31e153e77761d9c40826c55f30185
DekuCool24 <dayanna.qs@gmail.com>
2020-08-12 21:14:54 -0600

    Update Oreamuno_Cartago_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Oreamuno_Cartago_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Oreamuno_Cartago_central_opening_page.dart
@@ -8,6 +8,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Cipreses/Cipreses_Oreamuno_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Cot/Cot_Oreamuno_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Oreamuno_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Potrero_Cerrado/Cerrado_Oreamuno_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/San_Rafael/San_Rafael_Oreamuno_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Santa_Rosa/Santa_Rosa_Oreamuno_Cartago_StoryTelling.dart';
 
 class regionSocialEconomicaCentralOreamunoCartago extends StatelessWidget {
   @override
@@ -29,39 +35,70 @@ class regionSocialEconomicaCentralOreamunoCartago extends StatelessWidget {
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.map),
-            title: Text('Cerrado', style: TextStyle(fontSize: 25),
+            title: Text('Potrero Cerrado', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingPotreroCerradoOreamunoCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Cipreses', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingCipresesOreamunoCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Cot', style: TextStyle(fontSize: 25),
             ),
-          ),
-          ListTile(
-            leading: Icon(FontAwesomeIcons.trello),
-            title: Text('Potrero', style: TextStyle(fontSize: 25),
-            ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingCotOreamunoCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Rafael', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanRafaelOreamunoCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santa Rosa', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantaRosaOreamunoCartago.withSampleData()),
+              )
+            },
           ),
 
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingOreamunoCartago.withSampleData()),
+              )
+            },
           ),
         ],
       ),
