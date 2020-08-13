ff498c70b1aad0ca63c1a9f4581be23f6134b97e
DekuCool24 <dayanna.qs@gmail.com>
2020-08-12 21:14:37 -0600

    Update Turrialba_Cartago_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Turrialba_Cartago_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Turrialba_Cartago_central_opening_page.dart
@@ -8,6 +8,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Chirrip%C3%B3/Chirripo_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/La_Isabel/La_Isabel_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/La_Suiza/La_Suiza_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Pavones/Pavones_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Peralta/Peralta_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Santa_Cruz/Santa_Cruz_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Santa_Rosa/Santa_Rosa_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Santa_Teresita/Santa_Teresita_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Tayutic/Tayutic_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Tres_Equis/Tres_Equis_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Tuis/Tuis_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Turrialba/Turrialba_Turrialba_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Turrialba/Turrialba_Cartago_StoryTelling.dart';
 
 class regionSocialEconomicaCentralTurrialbaCartago extends StatelessWidget {
   @override
@@ -31,61 +44,133 @@ class regionSocialEconomicaCentralTurrialbaCartago extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Chirripó', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingChirripoTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('La Isabel', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingLaIsabelTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('La Suiza', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingLaSuizaTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Pavones', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingPavonesTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Peralta', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingPeraltaTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santa Cruz', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantaCruzTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santa Rosa', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantaRosaTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santa Teresita', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantaTeresitaTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Tayutic', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingTayuticTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Tres Equis', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingTresEquisTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Tuis', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingTuisTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Turrialba', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingTurrialbaTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
 
 
@@ -93,6 +178,12 @@ class regionSocialEconomicaCentralTurrialbaCartago extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingTurrialbaCartago.withSampleData()),
+              )
+            },
           ),
         ],
       ),
