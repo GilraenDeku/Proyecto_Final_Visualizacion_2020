8cb8b19f5670e5233fa24f214b5f57c83fdd2d2b
DekuCool24 <dayanna.qs@gmail.com>
2020-08-12 21:11:45 -0600

    Update San_Ramon_Alajuela_Storytelling.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/San_Ramón/San_Ramon_Alajuela_Storytelling.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Alajuela/San_Ramón/San_Ramon_Alajuela_Storytelling.dart
@@ -134,6 +134,7 @@ class storytellingSanRamonAlajuela extends StatelessWidget {
         children: [
           _buildButtonColumn(color, Icons.find_in_page, 'WEB'),
           _buildMuniButtonColumn(color, Icons.find_in_page, 'MUNICIPALIDAD'),
+          _buildMapaButtonColumn(color, Icons.find_in_page, 'MAPA'),
         ],
       ),
     );
@@ -421,6 +422,46 @@ class storytellingSanRamonAlajuela extends StatelessWidget {
       ],
     );
   }
+
+  Column _buildMapaButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+        GestureDetector(
+            child:
+            Icon(icon, color: color),
+            onTap: () {
+              // do what you need to do when "Click here" gets clicked
+              launch('https://www.hacienda.go.cr/docs/545cdbe2df9a6_22.pdf');
+            }
+        ),
+
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Center(
+            child:
+            GestureDetector(
+                child: Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: color,
+                  ),
+                ),
+                onTap: () {
+                  // do what you need to do when "Click here" gets clicked
+                  launch('https://www.hacienda.go.cr/docs/545cdbe2df9a6_22.pdf');
+                }
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
 
 }
 
