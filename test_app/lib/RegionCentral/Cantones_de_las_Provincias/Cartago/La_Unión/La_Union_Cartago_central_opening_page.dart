1945e3fcd38825273fefca1546069cc6287cb05e
DekuCool24 <dayanna.qs@gmail.com>
2020-08-12 21:15:10 -0600

    Update La_Union_Cartago_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Unión/La_Union_Cartago_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Unión/La_Union_Cartago_central_opening_page.dart
@@ -8,6 +8,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/Cartago/Dulce_Nombre/Dulce_Nombre_Cartago_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/Concepci%C3%B3n/Concepcion_La_Union_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/Dulce_Nombre/Dulce_Nombre_La_Union_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/La_Union_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/R%C3%ADo_Azul/Rio_Azul_La_Union_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/San_Diego/San_Diego_La_Union_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/San_Juan/San_Juan_La_Union_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/San_Rafael/San_Rafael_La_Union_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/San_Ram%C3%B3n/San_Ramon_La_Union_Cartago_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Cartago/La_Uni%C3%B3n/Tres_R%C3%ADos/Tres_Rios_La_Union_Cartago_StoryTelling.dart';
 
 class regionSocialEconomicaCentralLaUnionCartago extends StatelessWidget {
   @override
@@ -31,47 +41,101 @@ class regionSocialEconomicaCentralLaUnionCartago extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Concepción', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingConcepcionLaUnionCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Dulce Nombre', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingDulceNombreLaUnionCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Río Azul', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingRioAzulLaUnionCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Diego', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanDiegoLaUnionCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Juan', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanJuanLaUnionCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Rafael', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanRafaelLaUnionCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Ramón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanRamonLaUnionCartago.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Tres Ríos', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingTresRiosLaUnionCartago.withSampleData()),
+              )
+            },
           ),
 
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingLaUnionCartago.withSampleData()),
+              )
+            },
           ),
         ],
       ),
