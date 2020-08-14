6cfe2ae4da74833530f4f1e7495eec8fcafce4d7
DekuCool24 <dayanna.qs@gmail.com>
2020-08-14 00:27:46 -0600

    Update Barva_Heredia_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Barva/Barva_Heredia_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Barva/Barva_Heredia_central_opening_page.dart
@@ -8,6 +8,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Barva/Barva/Barva_Barva_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Barva/San_Jos%C3%A9_de_la_Monta%C3%B1a/San_Jose_de_la_Montana_Barva_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Barva/San_Pablo/San_Pablo_Barva_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Barva/San_Pedro/San_Pedro_Barva_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Barva/San_Roque/San_Roque_Barva_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Barva/Santa_Luc%C3%ADa/Santa_Lucia_Barva_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/StoryTelling_Heredia.dart';
 
 class regionSocialEconomicaCentralBarvaHeredia extends StatelessWidget {
   @override
@@ -31,37 +38,79 @@ class regionSocialEconomicaCentralBarvaHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Barva', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingBarvaBarvaHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San José de la Montaña', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanJosedelaMontanaBarvaHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Pablo', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanPabloBarvaHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Pedro', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanPedroBarvaHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Roque', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanRoqueBarvaHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santa Lucía', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantaLuciaBarvaHeredia.withSampleData()),
+              )
+            },
           ),
 
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingHeredia.withSampleData()),
+              )
+            },
           ),
         ],
       ),
