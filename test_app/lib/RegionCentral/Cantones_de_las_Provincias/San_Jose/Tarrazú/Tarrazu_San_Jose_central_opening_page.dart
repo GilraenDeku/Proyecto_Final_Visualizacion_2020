019b284d133435812500b28b9a2b0182c03ea7a8
DekuCool24 <dayanna.qs@gmail.com>
2020-08-14 00:31:31 -0600

    Update Tarrazu_San_Jose_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/San_Jose/Tarrazú/Tarrazu_San_Jose_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/San_Jose/Tarrazú/Tarrazu_San_Jose_central_opening_page.dart
@@ -8,6 +8,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/San_Jose/Tarraz%C3%BA/Tarrazu_San_Jose_StoryTelling.dart';
 
 class regionSocialEconomicaCentralTarrazuSanJose extends StatelessWidget {
   @override
@@ -31,16 +32,34 @@ class regionSocialEconomicaCentralTarrazuSanJose extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('San Carlos', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanCarlosTarrazuSanJose.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Lorenzo', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanLorenzoTarrazuSanJose.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Marcos', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanMarcosTarrazuSanJose.withSampleData()),
+              )
+            },
           ),
 
 
@@ -48,6 +67,12 @@ class regionSocialEconomicaCentralTarrazuSanJose extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingTarrazuSanJose.withSampleData()),
+              )
+            },
           ),
         ],
       ),
