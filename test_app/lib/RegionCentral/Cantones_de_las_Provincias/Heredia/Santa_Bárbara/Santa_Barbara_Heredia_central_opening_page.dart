599cafb8e9b24316c714b61233d9f2c7e41d1bb1
DekuCool24 <dayanna.qs@gmail.com>
2020-08-14 00:29:54 -0600

    Update Santa_Barbara_Heredia_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_Bárbara/Santa_Barbara_Heredia_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_Bárbara/Santa_Barbara_Heredia_central_opening_page.dart
@@ -8,6 +8,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_B%C3%A1rbara/Barrio_Jes%C3%BAs/Barrio_Jesus_Santa_Barbara_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_B%C3%A1rbara/Puraba/Puraba_Santa_Barbara_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_B%C3%A1rbara/San_Juan/San_Juan_Santa_Barbara_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_B%C3%A1rbara/San_Pedro/San_Pedro_Santa_Barbara_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_B%C3%A1rbara/Santa_B%C3%A1rbara/Santa_Barbara_Santa_Barbara_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_B%C3%A1rbara/Santo_Domingo/Santo_Domingo_Santa_Barbara_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santa_B%C3%A1rbara/storytellingSantaBarbaraHeredia.dart';
 
 class regionSocialEconomicaCentralSantaBarbaraHeredia extends StatelessWidget {
   @override
@@ -31,31 +38,67 @@ class regionSocialEconomicaCentralSantaBarbaraHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Barrio Jesús', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingBarrioJesusSantaBarbaraHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Puraba', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingPurabaSantaBarbaraHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Juan', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanJuanSantaBarbaraHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Pedro', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanPedroSantaBarbaraHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santa Bárbara', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantaBarbaraSantaBarbaraHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santo Domingo', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantoDomingoSantaBarbaraHeredia.withSampleData()),
+              )
+            },
           ),
 
 
@@ -64,6 +107,12 @@ class regionSocialEconomicaCentralSantaBarbaraHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantaBarbaraHeredia.withSampleData()),
+              )
+            },
           ),
         ],
       ),
