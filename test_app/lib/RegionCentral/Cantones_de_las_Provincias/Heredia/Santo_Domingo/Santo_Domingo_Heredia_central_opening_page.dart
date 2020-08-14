3ca4a4d7c6411a674da66f6453c03c8bf81a0147
DekuCool24 <dayanna.qs@gmail.com>
2020-08-14 00:30:15 -0600

    Update Santo_Domingo_Heredia_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Santo_Domingo_Heredia_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Santo_Domingo_Heredia_central_opening_page.dart
@@ -8,6 +8,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Par%C3%A1-San_Luis/Para-San_Luis_Santo_Domingo_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Paracito/Paracito_Santo_Domingo_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/San_Miguel/San_Miguel_Santo_Domingo_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/San_Vicente/San_Vicente_Santo_Domingo_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Santa_Rosa/Santa_Rosa_Santo_Domingo_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Santo_Domingo/Santo_Domingo_Santo_Domingo_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Santo_Domingo_Heredia_StoryTelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Santo_Tom%C3%A1s/Santo_Tomas_Santo_Domingo_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/Tures-Angeles/Tunes_Angeles_Santo_Domingo_Heredia_Storytelling.dart';
 
 class regionSocialEconomicaCentralSantoDomingoHeredia extends StatelessWidget {
   @override
@@ -31,41 +40,89 @@ class regionSocialEconomicaCentralSantoDomingoHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Paracito', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingParacitoSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Pará-San Luis', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingParaSanLuisSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Miguel', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanMiguelSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Vicente', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanVicenteSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santa Rosa', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantaRosaSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santo Domingo', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantoDomingoSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santo Tomás', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantoTomasSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Tures-Ángeles', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingTuresAngelesSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
 
 
@@ -74,6 +131,12 @@ class regionSocialEconomicaCentralSantoDomingoHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantoDomingoHeredia.withSampleData()),
+              )
+            },
           ),
         ],
       ),
