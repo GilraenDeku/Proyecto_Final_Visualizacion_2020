3d46adf66e271600ed1bdc0cab483c6862eb808d
DekuCool24 <dayanna.qs@gmail.com>
2020-08-14 00:31:47 -0600

    Update Vazquez_de_Coronado_San_Jose_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/San_Jose/Vásquez_de_Coronado/Vazquez_de_Coronado_San_Jose_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/San_Jose/Vásquez_de_Coronado/Vazquez_de_Coronado_San_Jose_central_opening_page.dart
@@ -8,6 +8,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/San_Jose/V%C3%A1squez_de_Coronado/Vasquez_de_Coronado_San_Jose_StoryTelling.dart';
 
 class regionSocialEconomicaCentralVazquezdeCoronadoSanJose extends StatelessWidget {
   @override
@@ -19,7 +20,7 @@ class regionSocialEconomicaCentralVazquezdeCoronadoSanJose extends StatelessWidg
           DrawerHeader(
             child: Center (
               child: Text(
-                'Distritos del Cantón de Vásquez de Coronado',
+                'Distritos del Cantón de Vázquez de Coronado',
                 style: TextStyle(color: Colors.white, fontSize: 25),
                 textAlign: TextAlign.center,
               ),
@@ -31,26 +32,56 @@ class regionSocialEconomicaCentralVazquezdeCoronadoSanJose extends StatelessWidg
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Cascajal', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingCascajalVasquezdeCoronadoSanJose.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Dulce Nombre de Jesús', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingDulceNombredeJesusVasquezdeCoronadoSanJose.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Patalillo', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingPatalilloVasquezdeCoronadoSanJose.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Isidro', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanIsidroVasquezdeCoronadoSanJose.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Rafael', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanRafaelVasquezdeCoronadoSanJose.withSampleData()),
+              )
+            },
           ),
 
 
@@ -58,6 +89,12 @@ class regionSocialEconomicaCentralVazquezdeCoronadoSanJose extends StatelessWidg
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingVazquezdeCoronadoSanJose.withSampleData()),
+              )
+            },
           ),
         ],
       ),
