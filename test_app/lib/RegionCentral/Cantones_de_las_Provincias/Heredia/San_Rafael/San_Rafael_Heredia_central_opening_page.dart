89bf6034e04eed499afc38e4f0c30b243daa0785
DekuCool24 <dayanna.qs@gmail.com>
2020-08-14 00:29:37 -0600

    Update San_Rafael_Heredia_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Rafael/San_Rafael_Heredia_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Rafael/San_Rafael_Heredia_central_opening_page.dart
@@ -8,6 +8,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Rafael/Concepci%C3%B3n/Concepcion_San_Rafael_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Rafael/Los_Angeles/Los_Angeles_San_Rafael_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Rafael/San_Josecito/San_Josecito_San_Rafael_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Rafael/San_Rafael/San_Rafael_San_Rafael_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Rafael/Santiago/Santiago_San_Rafael_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Rafael/storytellingSanRafaelHeredia.dart';
 
 class regionSocialEconomicaCentralSanRafaelHeredia extends StatelessWidget {
   @override
@@ -31,26 +37,56 @@ class regionSocialEconomicaCentralSanRafaelHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Concepción', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingConcepcionSanRafaelHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Los Ángeles', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingLosAngelesSanRafaelHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Josecito', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanJosecitoSanRafaelHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Rafael', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanRafaelSanRafaelHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Santiago', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSantiagoSanRafaelHeredia.withSampleData()),
+              )
+            },
           ),
 
 
@@ -58,6 +94,12 @@ class regionSocialEconomicaCentralSanRafaelHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanRafaelHeredia.withSampleData()),
+              )
+            },
           ),
         ],
       ),
