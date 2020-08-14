0ff51f8dd31665e42ad0d5fa18423af936576643
DekuCool24 <dayanna.qs@gmail.com>
2020-08-14 00:29:15 -0600

    Update San_Isidro_Heredia_central_opening_page.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Isidro/San_Isidro_Heredia_central_opening_page.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Isidro/San_Isidro_Heredia_central_opening_page.dart
@@ -8,6 +8,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/Heredia/San_Francisco/San_Francisco_Heredia_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Isidro/Concepci%C3%B3n/Concepcion_San_Isidro_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Isidro/San_Isidro/San_Isidro_San_Isidro_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Isidro/San_Jos%C3%A9/San_Jose_San_Isidro_Heredia_Storytelling.dart';
+import 'package:testapp/RegionCentral/Cantones_de_las_Provincias/Heredia/San_Isidro/storytellingSanIsidroHeredia.dart';
 
 class regionSocialEconomicaCentralSanIsidroHeredia extends StatelessWidget {
   @override
@@ -31,21 +36,45 @@ class regionSocialEconomicaCentralSanIsidroHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.map),
             title: Text('Concepción', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingConcepcionSanIsidroHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Francisco', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanFranciscoHerediaHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San Isidro', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanIsidroSanIsidroHeredia.withSampleData()),
+              )
+            },
           ),
           ListTile(
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('San José', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanJoseSanIsidroHeredia.withSampleData()),
+              )
+            },
           ),
 
 
@@ -53,6 +82,12 @@ class regionSocialEconomicaCentralSanIsidroHeredia extends StatelessWidget {
             leading: Icon(FontAwesomeIcons.trello),
             title: Text('Storytelling del Cantón', style: TextStyle(fontSize: 25),
             ),
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => storytellingSanIsidroHeredia.withSampleData()),
+              )
+            },
           ),
         ],
       ),
