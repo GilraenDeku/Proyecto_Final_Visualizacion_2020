5a13731b534f25b4e240637655cd0ab2084349ff
DekuCool24 <dayanna.qs@gmail.com>
2020-08-17 00:44:03 -0600

    Update Cot_Oreamuno_Cartago_StoryTelling.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Cot/Cot_Oreamuno_Cartago_StoryTelling.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Oreamuno/Cot/Cot_Oreamuno_Cartago_StoryTelling.dart
@@ -206,30 +206,53 @@ class storytellingCotOreamunoCartago extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 20),
-                /*
                 Text(
-                    'Las regiones socioeconómicas de Costa Rica '
-                        '(a menudo denominadas sólo como regiones funcionales) '
-                        'son una subdivisión político-económica en la que '
-                        'se ha delimitado este país centroamericano. Esta subdivisión '
-                        'fue realizada por Decreto Ejecutivo Nº 7944 '
-                        'del 26 de enero de 1978.',
+                    'En la actualidad vivimos en un mundo en donde cada vez '
+                        'el uso de la tecnología se hace más indispensable, '
+                        'no solo para entretenernos sino también para '
+                        'comunicarnos, informarnos y trabajar, es por eso '
+                        'que es fundamental comprender el nivel de acceso '
+                        'que tiene a la tecnología: como lo es el radio, '
+                        'teléfonos, computadoras, televisores y el internet,'
+                        ' para así analizar la brecha tecnológica que '
+                        'existe y lograr crear proyectos que abarquen más'
+                        ' a la población',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
                 SizedBox(height: 20),
                 Text(
-                    'Estas regiones son seis en total: Región Central, '
-                        'Región Chorotega, Región Pacífico Central, '
-                        'Región Brunca, Región Huetar Atlántica y '
-                        'Región Huetar Norte. Algunos nombres de '
-                        'región se derivan de las etnias precolombinas '
-                        'que habitaron en esas zonas geográficas. ',
+                    'En el distrito de Cot se observa que el 86% de las '
+                        'casas cuentan con radio o equipos de sonido, el 48% con'
+                        ' línea de teléfono fija, 90% con teléfono celular, 13%'
+                        ' con pantalla de televisor, de este 13% el 95% de los '
+                        'televisores son convencionales y el 27% son con cable '
+                        'o satélite, el 29% tienen una computadora de escritorio,'
+                        ' 15% con una computadora portátil y el 15% de las casas'
+                        ' cuentan con acceso de internet.',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
-
-                 */
+                SizedBox(height: 20),
+                Text(
+                    'Cabe destacar que, como en el caso de las pantallas'
+                        ' de televisor, varias de las casas cuentan con más'
+                        ' de un tipo de tecnología por lo cual los índices'
+                        ' son más altos. ',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
+                SizedBox(height: 20),
+                Text(
+                    'En conclusión, en el caso de este distrito el uso de'
+                        ' tecnologías es muy reducido constando así '
+                        'mayoritariamente del uso de radios o equipos de'
+                        ' sonido y telefonía celular, fija y computadora '
+                        'de escritorio siendo el teléfono celular la '
+                        'tecnología de mayor uso.',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
               ],
             ),
           ),
@@ -280,9 +303,7 @@ class storytellingCotOreamunoCartago extends StatelessWidget {
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
                                     Image.asset(
-                                      'assets/regionSocioeconomicaGrafrico.jpg',
-                                      width: 450,
-                                      height: 400,
+                                      'assets/InformacionGraficoSanJose.png',
                                       fit: BoxFit.cover,
                                     ),
                                   ],
@@ -356,7 +377,7 @@ class storytellingCotOreamunoCartago extends StatelessWidget {
             Icon(icon, color: color),
             onTap: () {
               // do what you need to do when "Click here" gets clicked
-              launch('https://launion.go.cr/');
+              launch('https://www.oreamuno.go.cr/');
             }
         ),
 
@@ -375,7 +396,7 @@ class storytellingCotOreamunoCartago extends StatelessWidget {
                 ),
                 onTap: () {
                   // do what you need to do when "Click here" gets clicked
-                  launch('https://launion.go.cr/');
+                  launch('https://www.oreamuno.go.cr/');
                 }
             ),
           ),
@@ -395,9 +416,11 @@ class storytellingCotOreamunoCartago extends StatelessWidget {
             Icon(icon, color: color),
             onTap: () {
               // do what you need to do when "Click here" gets clicked
-              launch('https://www.hacienda.go.cr/docs/5812418d2593d_'
-                  'Mapa%20del%20Canton%20La%20Union%2003%20'
-                  'Distrito%2001%20a%2008.pdf');
+              launch('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source'
+                  '=web&cd=&cad=rja&uact=8&ved=2ahUKEwjfpa6RsaHrAhWs1FkKHdVu'
+                  'DDQQFjAQegQIBxAB&url=https%3A%2F%2Fwww.hacienda.go.cr%2Fd'
+                  'ocs%2F544rtwertwrtwe5rt4wrt5w4rtwe4rt47.pdf&usg=AOvVaw3W0'
+                  'PwUl-8PeVM-MdquOpgh');
             }
         ),
 
@@ -416,9 +439,11 @@ class storytellingCotOreamunoCartago extends StatelessWidget {
                 ),
                 onTap: () {
                   // do what you need to do when "Click here" gets clicked
-                  launch('https://www.hacienda.go.cr/docs/5812418d2593d_Mapa'
-                      '%20del%20Canton%20La%20Union%2003%20'
-                      'Distrito%2001%20a%2008.pdf');
+                  launch('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source'
+                      '=web&cd=&cad=rja&uact=8&ved=2ahUKEwjfpa6RsaHrAhWs1FkKH'
+                      'dVuDDQQFjAQegQIBxAB&url=https%3A%2F%2Fwww.hacienda.go.'
+                      'cr%2Fdocs%2F544rtwertwrtwe5rt4wrt5w4rtwe4rt47.pdf&u'
+                      'sg=AOvVaw3W0PwUl-8PeVM-MdquOpgh');
                 }
             ),
           ),
