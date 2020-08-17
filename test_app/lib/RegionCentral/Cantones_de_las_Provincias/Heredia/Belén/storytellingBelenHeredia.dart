ecbb801394ff96c1b14c9aadac34ec9329050937
DekuCool24 <dayanna.qs@gmail.com>
2020-08-17 00:45:27 -0600

    Update storytellingBelenHeredia.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Belén/storytellingBelenHeredia.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Belén/storytellingBelenHeredia.dart
@@ -126,7 +126,6 @@ class storytellingBelenHeredia extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(color, Icons.find_in_page, 'WEB'),
           _buildMuniButtonColumn(color, Icons.find_in_page, 'MUNICIPALIDAD'),
           _buildMapaButtonColumn(color, Icons.find_in_page, 'MAPA'),
         ],
@@ -198,30 +197,55 @@ class storytellingBelenHeredia extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 20),
-                /*
                 Text(
-                    'Las regiones socioeconómicas de Costa Rica '
-                        '(a menudo denominadas sólo como regiones funcionales) '
-                        'son una subdivisión político-económica en la que '
-                        'se ha delimitado este país centroamericano. Esta subdivisión '
-                        'fue realizada por Decreto Ejecutivo Nº 7944 '
-                        'del 26 de enero de 1978.',
+                    'En la actualidad vivimos en un mundo en donde cada vez '
+                        'el uso de la tecnología se hace más indispensable, '
+                        'no solo para entretenernos sino también para '
+                        'comunicarnos, informarnos y trabajar, es por eso '
+                        'que es fundamental comprender el nivel de acceso '
+                        'que tiene a la tecnología: como lo es el radio, '
+                        'teléfonos, computadoras, televisores y el internet,'
+                        ' para así analizar la brecha tecnológica que '
+                        'existe y lograr crear proyectos que abarquen más'
+                        ' a la población',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
                 SizedBox(height: 20),
                 Text(
-                    'Estas regiones son seis en total: Región Central, '
-                        'Región Chorotega, Región Pacífico Central, '
-                        'Región Brunca, Región Huetar Atlántica y '
-                        'Región Huetar Norte. Algunos nombres de '
-                        'región se derivan de las etnias precolombinas '
-                        'que habitaron en esas zonas geográficas. ',
+                    'En el cantón de Belén se observa que el 86% de las '
+                        'casas cuentan con radio o equipos de sonido, el 75% con'
+                        ' línea de teléfono fija, 94% con teléfono celular, 39%'
+                        ' con pantalla de televisor, de este 39% el 89% de los '
+                        'televisores son convencionales y el 73% son con cable '
+                        'o satélite, el 49% tienen una computadora de escritorio,'
+                        ' 48% con una computadora portátil y el 59% de las casas'
+                        ' cuentan con acceso de internet.',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
-
-                 */
+                SizedBox(height: 20),
+                Text(
+                    'Cabe destacar que, como en el caso de las pantallas'
+                        ' de televisor, varias de las casas cuentan con más'
+                        ' de un tipo de tecnología por lo cual los índices'
+                        ' son más altos. ',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
+                SizedBox(height: 20),
+                Text(
+                    'En conclusión, se puede observar que la tecnología que más'
+                        ' utiliza la población son los teléfonos celulares los'
+                        ' cuales van en un aumento considerable. A pesar de que'
+                        ' los celulares son los que más utiliza la población, '
+                        'no se recomienda enfocarse solo a esta tecnología ya '
+                        'que los índices de uso de las demás tecnologías son '
+                        'muy altos, lo que indica que en el cantón de Belén'
+                        ' el uso de las tecnologías es bastante grande.',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
               ],
             ),
           ),
@@ -273,9 +297,7 @@ class storytellingBelenHeredia extends StatelessWidget {
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
                                     Image.asset(
-                                      'assets/BelenCanton.png',
-                                      width: 450,
-                                      height: 400,
+                                      'assets/InformacionGraficoSanJose.png',
                                       fit: BoxFit.cover,
                                     ),
                                   ],
@@ -336,46 +358,6 @@ class storytellingBelenHeredia extends StatelessWidget {
         labelAccessorFn: (LinearSales row, _) => '${row.sales}',
       )
     ];
-  }
-
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-
-        GestureDetector(
-            child:
-            Icon(icon, color: color),
-            onTap: () {
-              // do what you need to do when "Click here" gets clicked
-              launch('https://es.wikipedia.org/wiki/Cant%C3%B3n_de_Bel%C3%A9n');
-            }
-        ),
-
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Center(
-            child:
-            GestureDetector(
-                child: Text(
-                  label,
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: color,
-                  ),
-                ),
-                onTap: () {
-                  // do what you need to do when "Click here" gets clicked
-                  launch('https://es.wikipedia.org/wiki/Cant%C3%B3n_de_Bel%C3%A9n');
-                }
-            ),
-          ),
-        ),
-      ],
-    );
   }
 
   Column _buildMuniButtonColumn(Color color, IconData icon, String label) {
