f8811816494d5f28130dfca6ed79e6ff72f2c94e
DekuCool24 <dayanna.qs@gmail.com>
2020-08-17 00:46:38 -0600

    Update San_Miguel_Santo_Domingo_Heredia_Storytelling.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/San_Miguel/San_Miguel_Santo_Domingo_Heredia_Storytelling.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Santo_Domingo/San_Miguel/San_Miguel_Santo_Domingo_Heredia_Storytelling.dart
@@ -134,8 +134,8 @@ class storytellingSanMiguelSantoDomingoHeredia extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(color, Icons.find_in_page, 'MAPA'),
           _buildMuniButtonColumn(color, Icons.find_in_page, 'MUNICIPALIDAD'),
+          _buildButtonColumn(color, Icons.find_in_page, 'MAPA'),
         ],
       ),
     );
@@ -205,30 +205,56 @@ class storytellingSanMiguelSantoDomingoHeredia extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 20),
-                /*
                 Text(
-                    'Las regiones socioeconómicas de Costa Rica '
-                        '(a menudo denominadas sólo como regiones funcionales) '
-                        'son una subdivisión político-económica en la que '
-                        'se ha delimitado este país centroamericano. Esta subdivisión '
-                        'fue realizada por Decreto Ejecutivo Nº 7944 '
-                        'del 26 de enero de 1978.',
+                    'En la actualidad vivimos en un mundo en donde cada vez '
+                        'el uso de la tecnología se hace más indispensable, '
+                        'no solo para entretenernos sino también para '
+                        'comunicarnos, informarnos y trabajar, es por eso '
+                        'que es fundamental comprender el nivel de acceso '
+                        'que tiene a la tecnología: como lo es el radio, '
+                        'teléfonos, computadoras, televisores y el internet,'
+                        ' para así analizar la brecha tecnológica que '
+                        'existe y lograr crear proyectos que abarquen más'
+                        ' a la población',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
                 SizedBox(height: 20),
                 Text(
-                    'Estas regiones son seis en total: Región Central, '
-                        'Región Chorotega, Región Pacífico Central, '
-                        'Región Brunca, Región Huetar Atlántica y '
-                        'Región Huetar Norte. Algunos nombres de '
-                        'región se derivan de las etnias precolombinas '
-                        'que habitaron en esas zonas geográficas. ',
+                    'En el distrito de San Miguel se observa que el '
+                        '88% de las '
+                        'casas cuentan con radio o equipos de sonido, el 75% con'
+                        ' línea de teléfono fija, 92% con teléfono celular, 29%'
+                        ' con pantalla de televisor, de este 29% el 94% de los '
+                        'televisores son convencionales y el 56% son con cable '
+                        'o satélite, el 51% tienen una computadora de escritorio,'
+                        ' 39% con una computadora portátil y el 51% de las casas'
+                        ' cuentan con acceso de internet.',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
-
-                 */
+                SizedBox(height: 20),
+                Text(
+                    'Cabe destacar que, como en el caso de las pantallas'
+                        ' de televisor, varias de las casas cuentan con más'
+                        ' de un tipo de tecnología por lo cual los índices'
+                        ' son más altos. ',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
+                SizedBox(height: 20),
+                Text(
+                    'En conclusión, se puede observar que la tecnología que más'
+                        ' utiliza la población son los teléfonos celulares los'
+                        ' cuales van en un aumento considerable. A pesar de que'
+                        ' los celulares son los que más utiliza la población, no'
+                        ' se recomienda enfocarse solo a esta tecnología ya que'
+                        ' los índices de uso de las demás tecnologías son muy '
+                        'altos, lo que indica que en el distrito de San Miguel el'
+                        ' uso de las tecnologías es bastante grande.',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
               ],
             ),
           ),
@@ -249,7 +275,7 @@ class storytellingSanMiguelSantoDomingoHeredia extends StatelessWidget {
               body: ListView(
                 children: [
                   Image.asset(
-                    'assets/SantoDomingoDistritos.jpg',
+                    'assets/InformacionGraficoSanJose.png',
                     fit: BoxFit.cover,
                   ),
                   titleSection,
