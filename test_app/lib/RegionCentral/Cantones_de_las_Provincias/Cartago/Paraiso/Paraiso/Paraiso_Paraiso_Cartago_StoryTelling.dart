7931929a4dbe1e7575d6490222257b70d2005541
DekuCool24 <dayanna.qs@gmail.com>
2020-08-17 00:44:17 -0600

    Update Paraiso_Paraiso_Cartago_StoryTelling.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Paraiso/Paraiso_Paraiso_Cartago_StoryTelling.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Cartago/Paraiso/Paraiso/Paraiso_Paraiso_Cartago_StoryTelling.dart
@@ -36,7 +36,7 @@ class storytellingParaisoParaisoCartago extends StatelessWidget {
                   padding: const EdgeInsets.only(bottom: 8),
                   child: Center(
                     child: Text(
-                      'Distrito de Paraiso',
+                      'Distrito de Paraíso',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
@@ -48,7 +48,7 @@ class storytellingParaisoParaisoCartago extends StatelessWidget {
                   padding: const EdgeInsets.only(bottom: 8),
                   child: Center(
                     child: Text(
-                      'Cantón de Paraiso, Provincia de Cartago, Región Central',
+                      'Cantón de Paraíso, Provincia de Cartago, Región Central',
                       style: TextStyle(
                         color: Colors.grey[500],
                       ),
@@ -206,30 +206,55 @@ class storytellingParaisoParaisoCartago extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 20),
-                /*
                 Text(
-                    'Las regiones socioeconómicas de Costa Rica '
-                        '(a menudo denominadas sólo como regiones funcionales) '
-                        'son una subdivisión político-económica en la que '
-                        'se ha delimitado este país centroamericano. Esta subdivisión '
-                        'fue realizada por Decreto Ejecutivo Nº 7944 '
-                        'del 26 de enero de 1978.',
+                    'En la actualidad vivimos en un mundo en donde cada vez '
+                        'el uso de la tecnología se hace más indispensable, '
+                        'no solo para entretenernos sino también para '
+                        'comunicarnos, informarnos y trabajar, es por eso '
+                        'que es fundamental comprender el nivel de acceso '
+                        'que tiene a la tecnología: como lo es el radio, '
+                        'teléfonos, computadoras, televisores y el internet,'
+                        ' para así analizar la brecha tecnológica que '
+                        'existe y lograr crear proyectos que abarquen más'
+                        ' a la población',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
                 SizedBox(height: 20),
                 Text(
-                    'Estas regiones son seis en total: Región Central, '
-                        'Región Chorotega, Región Pacífico Central, '
-                        'Región Brunca, Región Huetar Atlántica y '
-                        'Región Huetar Norte. Algunos nombres de '
-                        'región se derivan de las etnias precolombinas '
-                        'que habitaron en esas zonas geográficas. ',
+                    'En el distrito de Paraíso se observa que el 90% de las '
+                        'casas cuentan con radio o equipos de sonido, el 69% con'
+                        ' línea de teléfono fija, 91% con teléfono celular, 23%'
+                        ' con pantalla de televisor, de este 23% el 96% de los '
+                        'televisores son convencionales y el 42% son con cable '
+                        'o satélite, el 44% tienen una computadora de escritorio,'
+                        ' 30% con una computadora portátil y el 42% de las casas'
+                        ' cuentan con acceso de internet.',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
-
-                 */
+                SizedBox(height: 20),
+                Text(
+                    'Cabe destacar que, como en el caso de las pantallas'
+                        ' de televisor, varias de las casas cuentan con más'
+                        ' de un tipo de tecnología por lo cual los índices'
+                        ' son más altos. ',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
+                SizedBox(height: 20),
+                Text(
+                    'En conclusión, se puede observar que la tecnología que más'
+                        ' utiliza la población son los teléfonos celulares los '
+                        'cuales van en un aumento considerable. A pesar de que '
+                        'los celulares son los que más utiliza la población, no'
+                        ' se recomienda enfocarse solo a esta tecnología ya que'
+                        ' los índices de uso de las demás tecnologías son muy '
+                        'altos, lo que indica que en el distrito de Paraíso el uso'
+                        ' de las tecnologías es bastante grande.',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
               ],
             ),
           ),
@@ -245,7 +270,7 @@ class storytellingParaisoParaisoCartago extends StatelessWidget {
             child: Scaffold(
               appBar: AppBar(
                 backgroundColor: Color(0xff1976d2),
-                title: Text('Distrito de Paraiso'),
+                title: Text('Distrito de Paraíso'),
               ),
               body: ListView(
                 children: [
@@ -280,9 +305,7 @@ class storytellingParaisoParaisoCartago extends StatelessWidget {
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
                                     Image.asset(
-                                      'assets/regionSocioeconomicaGrafrico.jpg',
-                                      width: 450,
-                                      height: 400,
+                                      'assets/InformacionGraficoSanJose.png',
                                       fit: BoxFit.cover,
                                     ),
                                   ],
