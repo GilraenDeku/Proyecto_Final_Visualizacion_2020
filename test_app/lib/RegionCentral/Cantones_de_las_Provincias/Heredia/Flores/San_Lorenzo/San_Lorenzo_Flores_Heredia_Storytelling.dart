78586954263c358600d2e0c165fea90b4da0a370
DekuCool24 <dayanna.qs@gmail.com>
2020-08-17 00:45:51 -0600

    Update San_Lorenzo_Flores_Heredia_Storytelling.dart

--- test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Flores/San_Lorenzo/San_Lorenzo_Flores_Heredia_Storytelling.dart
+++ test_app/lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Flores/San_Lorenzo/San_Lorenzo_Flores_Heredia_Storytelling.dart
@@ -134,8 +134,8 @@ class storytellingSanLorenzoFloresHeredia extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(color, Icons.find_in_page, 'MAPA'),
           _buildMuniButtonColumn(color, Icons.find_in_page, 'MUNICIPALIDAD'),
+          _buildButtonColumn(color, Icons.find_in_page, 'MAPA'),
         ],
       ),
     );
@@ -205,30 +205,55 @@ class storytellingSanLorenzoFloresHeredia extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 20),
-                /*
                 Text(
-                    'Las regiones socioeconómicas de Costa Rica '
-                        '(a menudo denominadas sólo como regiones funcionales) '
-                        'son una subdivisión político-económica en la que '
-                        'se ha delimitado este país centroamericano. Esta subdivisión '
-                        'fue realizada por Decreto Ejecutivo Nº 7944 '
-                        'del 26 de enero de 1978.',
+                    'En la actualidad vivimos en un mundo en donde cada vez '
+                        'el uso de la tecnología se hace más indispensable, '
+                        'no solo para entretenernos sino también para '
+                        'comunicarnos, informarnos y trabajar, es por eso '
+                        'que es fundamental comprender el nivel de acceso '
+                        'que tiene a la tecnología: como lo es el radio, '
+                        'teléfonos, computadoras, televisores y el internet,'
+                        ' para así analizar la brecha tecnológica que '
+                        'existe y lograr crear proyectos que abarquen más'
+                        ' a la población',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
                 SizedBox(height: 20),
                 Text(
-                    'Estas regiones son seis en total: Región Central, '
-                        'Región Chorotega, Región Pacífico Central, '
-                        'Región Brunca, Región Huetar Atlántica y '
-                        'Región Huetar Norte. Algunos nombres de '
-                        'región se derivan de las etnias precolombinas '
-                        'que habitaron en esas zonas geográficas. ',
+                    'En el distrito de San Lorenzo/Barrantes se observa que el 86% de las '
+                        'casas cuentan con radio o equipos de sonido, el 80% con'
+                        ' línea de teléfono fija, 91% con teléfono celular, 46%'
+                        ' con pantalla de televisor, de este 46% el 88% de los '
+                        'televisores son convencionales y el 73% son con cable '
+                        'o satélite, el 51% tienen una computadora de escritorio,'
+                        ' 54% con una computadora portátil y el 66% de las casas'
+                        ' cuentan con acceso de internet.',
                     softWrap: true,
                     style: TextStyle(fontSize: 18),
                     textAlign: TextAlign.justify),
-
-                 */
+                SizedBox(height: 20),
+                Text(
+                    'Cabe destacar que, como en el caso de las pantallas'
+                        ' de televisor, varias de las casas cuentan con más'
+                        ' de un tipo de tecnología por lo cual los índices'
+                        ' son más altos. ',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
+                SizedBox(height: 20),
+                Text(
+                    'En conclusión, se puede observar que la tecnología que más'
+                        ' utiliza la población son los teléfonos celulares los'
+                        ' cuales van en un aumento considerable. A pesar de que'
+                        ' los celulares son los que más utiliza la población, '
+                        'no se recomienda enfocarse solo a esta tecnología ya '
+                        'que los índices de uso de las demás tecnologías son '
+                        'muy altos, lo que indica que en el distrito de San Lorenzo/Barrantes'
+                        ' el uso de las tecnologías es bastante grande.',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
               ],
             ),
           ),
@@ -280,9 +305,7 @@ class storytellingSanLorenzoFloresHeredia extends StatelessWidget {
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
                                     Image.asset(
-                                      'assets/regionSocioeconomicaGrafrico.jpg',
-                                      width: 450,
-                                      height: 400,
+                                      'assets/InformacionGraficoSanJose.png',
                                       fit: BoxFit.cover,
                                     ),
                                   ],
